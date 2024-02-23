6e9a06aa659dc7b6ec6ffbc35be3e079a96b7fcb
Kornilia <114872039+SophiaKornilia@users.noreply.github.com>
2024-02-23 11:20:08 +0100

    colorAll

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -4,16 +4,24 @@ import "../styles/quiz.css";
 export const Quiz = () => {
   const [points, setPoints] = useState(0);
   const [show, setShow] = useState(false);
-  
-const randomColor = () : string => {
-        return '#' + Math.floor(Math.random() * 0xffffff).toString(16).padStart(6, '0');
-}
+
+  const randomColor = (): string => {
+    return (
+      "#" +
+      Math.floor(Math.random() * 0xffffff)
+        .toString(16)
+        .padStart(6, "0")
+    );
+  };
 
   const handleChange = () => {
     setPoints(points + 1);
     document.body.style.backgroundColor = randomColor();
   };
-  
+
+  const handleChange2 = () => {
+    document.body.style.backgroundColor = randomColor();
+  };
 
   const handleClick = () => {
     setShow(true);
@@ -29,17 +37,21 @@ const randomColor = () : string => {
           <div className="answers">
             <input type="radio" name="q1" onChange={handleChange} />{" "}
             <p>a 1889</p>
-            <input type="radio" name="q1" /> <p>b 1901</p>
-            <input type="radio" name="q1" /> <p>c 1915</p>
+            <input type="radio" name="q1" onChange={handleChange2} />{" "}
+            <p>b 1901</p>
+            <input type="radio" name="q1" onChange={handleChange2} />{" "}
+            <p>c 1915</p>
           </div>
         </div>
         <div className="unit">
           <h3>Vilket land är känt för att vara födelseplatsen för pizza?</h3>
           <div className="answers">
-            <input type="radio" name="q2" /> <p>a Spanien</p>
+            <input type="radio" name="q2" onChange={handleChange2} />{" "}
+            <p>a Spanien</p>
             <input type="radio" name="q2" onChange={handleChange} />{" "}
             <p>b Italien</p>
-            <input type="radio" name="q2" /> <p>c Grekland</p>
+            <input type="radio" name="q2" onChange={handleChange2} />{" "}
+            <p>c Grekland</p>
           </div>
         </div>
         <div className="unit">
@@ -47,15 +59,19 @@ const randomColor = () : string => {
           <div className="answers">
             <input type="radio" name="q3" onChange={handleChange} />{" "}
             <p>a Astrid Lindgren</p>
-            <input type="radio" name="q3" /> <p>b Roald Dahl</p>
-            <input type="radio" name="q3" /> <p>c J.K. Rowling</p>
+            <input type="radio" name="q3" onChange={handleChange2} />{" "}
+            <p>b Roald Dahl</p>
+            <input type="radio" name="q3" onChange={handleChange2} />{" "}
+            <p>c J.K. Rowling</p>
           </div>
         </div>
         <div className="unit">
           <h3></h3>Vilket är världens största ocean?
           <div className="answers">
-            <input type="radio" name="q4" /> <p>a Indiska oceanen</p>
-            <input type="radio" name="q4" /> <p>b Atlanten</p>
+            <input type="radio" name="q4" onChange={handleChange2} />{" "}
+            <p>a Indiska oceanen</p>
+            <input type="radio" name="q4" onChange={handleChange2} />{" "}
+            <p>b Atlanten</p>
             <input type="radio" name="q4" onChange={handleChange} />{" "}
             <p>c Stilla havet</p>
           </div>
@@ -65,8 +81,10 @@ const randomColor = () : string => {
           <div className="answers">
             <input type="radio" name="q5" onChange={handleChange} />{" "}
             <p>a 1939</p>
-            <input type="radio" name="q5" /> <p>b 1941</p>
-            <input type="radio" name="q5" /> <p>c 1945</p>
+            <input type="radio" name="q5" onChange={handleChange2} />{" "}
+            <p>b 1941</p>
+            <input type="radio" name="q5" onChange={handleChange2} />{" "}
+            <p>c 1945</p>
           </div>
           <button onClick={handleClick}>Visa resultat</button>
           {show && (
